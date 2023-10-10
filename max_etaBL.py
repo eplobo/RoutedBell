8f05e2e963d8de6319e0bb668560bcde243ef3d4
Edwin Lobo <120175710+eplobo@users.noreply.github.com>
2023-10-10 15:11:41 +0200

    Update and rename max_etaLv3_observables.py to max_etaBL.py
    Added annotations

--- max_etaBL.py
+++ max_etaBL.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep  4 15:10:46 2023
-
 @author: Edwin
 """
-    
+"""
+This code runs the NPA implementations for scenarios considered in the manuscript.
+
+NOTE: This code requires the latest version of the package ncpol2sdpa that is being 
+maintained by Peter Brown and is available on github.
+
+Details about the implementation can be found in the Appendix of the manuscript.
+"""
 def get_momentequalities(A,B,D,etaA,etaBS,etaBL,chsh_or_zx):
+    ### Get the moment equalities ###
     equalities = []
     v = visibility
     if binning:
@@ -76,10 +83,14 @@ def get_momentequalities(A,B,D,etaA,etaBS,etaBL,chsh_or_zx):
 
     
 def objective():
+    """
+    Define the objective function. Since ncpol2sdpa minimizes the objective by default, we use -etaBL
+    """
     return -etaBL
 
 
 def get_subs(A,B,D):
+    ### Substitutions used in the moment matrix ##
     
     ## D0 and D1 commute##
     subs = {D[1]*D[0]:D[0]*D[1]}
@@ -108,27 +119,28 @@ def get_subs(A,B,D):
 """
 Now we start with setting up the ncpol2sdpa computations
 """
+
 import numpy as np
 from math import sqrt
 import ncpol2sdpa as ncp
 import matplotlib.pyplot as plt
 
 
-nA = 2
+nA = 2 
 nB = 2
 nX = 2
 nY = 2
 nZ = 2
 d=2
-chsh_or_zx = 'chsh'
-binning = False
+chsh_or_zx = 'chsh' ##This fixes the ideal implementation to be used. For CHSH implementation, set this variable equal to the string 'chsh'. For the BB84 implementation, set this variable equal to the string 'zx'.
+binning = False ## Choose whether the outcomes are deterministically binned to +1 or if they are kept as seperate outcomes
 visibility = 0.94
 
 if chsh_or_zx == 'chsh':
     if binning:
-        etaA_list = [1]#np.linspace(0.96,1,20)
+        etaA_list = np.linspace(0.96,1,20)
     else:
-        etaA_list = [1] # np.linspace(0.9,0.92,3).tolist() + np.linspace(0.92,0.94,4)[1:].tolist() + np.linspace(0.94,0.96,5)[1:].tolist()  + np.linspace(0.96,0.98,7)[1:].tolist() + \
+        etaA_list = np.linspace(0.9,0.92,3).tolist() + np.linspace(0.92,0.94,4)[1:].tolist() + np.linspace(0.94,0.96,5)[1:].tolist()  + np.linspace(0.96,0.98,7)[1:].tolist() + \
             # np.linspace(0.98,0.99,7)[1:].tolist() + np.linspace(0.99,1,8)[1:].tolist()
 
 if chsh_or_zx == 'zx':
@@ -136,9 +148,8 @@ if chsh_or_zx == 'zx':
         etaA_list = [1]#np.linspace(0.9586,1,10)
     else:
         etaA_list = [1] #np.linspace(0.8942,0.8955,5).tolist() + np.linspace(0.8955,1,10)[1:].tolist() 
-    
 
-
+### Define the observables used in the moment matrix ###
 
 A0 = ncp.generate_operators('A0',1,hermitian=True)[0]
 A1 = ncp.generate_operators('A1',1,hermitian=True)[0]
@@ -158,6 +169,8 @@ else:
 
 obj = objective()
 
+
+### Construct the operators to be used in the moment matrix ### 
 if binning:
     level1 = ncp.flatten([A,B,D])
     
@@ -308,7 +321,7 @@ else:
                                     
 
 subs = get_subs(A,B,D) 
-sdp = ncp.SdpRelaxation(ncp.flatten([level4]), normalized=True, verbose=False)
+sdp = ncp.SdpRelaxation(ncp.flatten([level4]), normalized=True, verbose=False) ## Set verbose = True to display the solver output. 
 if binning:
     t = 14 ##Number of independent elements in p(ab|xyz)
 else:
@@ -323,7 +336,11 @@ etaBL_list = []
 for etaA in etaA_list:
     etaBS = etaA
     sdp.process_constraints(momentequalities = get_momentequalities(A, B, D,etaA,etaBS,etaBL, chsh_or_zx))
-    # prob = sdp.convert_to_cvxpy()
+    """
+    If you are using Peter Wittek originial ncpol2sdpa package, and not the latest version by Peter Brown, you will have to first convert the problem 
+    to cvxpy and then solve using cvxpy. 
+    """
+    # prob = sdp.convert_to_cvxpy() 
     # prob.solve(verbose = False)
     sdp.solve()
     etaBL_list += [-sdp.primal]
@@ -332,220 +349,5 @@ for etaA in etaA_list:
     # print(etaA, -prob.value)
 
 
-# plt.plot(etaA_list,etaBL_list,'r-' , linewidth=2)
-# plt.grid(color='r', linestyle='-', linewidth=1)
-
-
-# # save etaA_list and etaBL_list to text file
-# np.savetxt('etaA_list.txt', etaA_list)
-# np.savetxt('etaBL_list.txt', etaBL_list)
-
-
-
-
-###### Discarded Code ### 
-"""
-def get_momentequalities(p_obs,A,B,D,etaA,etaBS,etaBL,chsh_or_zx):
-    equalities = []
-    if binning:
-        
-        for x in range(nX):
-            equalities += [A[x] - (1-etaA) ]      
-            
-        for y in range(nY):
-            equalities += [B[y] - (1-etaBS)]
-            equalities += [D[y] - (1-etaBL)]
-
-            
-        for x in range(nX):
-            for y in range(nY):
-                equalities += [A[x]*B[y] - ( etaA*etaBS* (-1)**(x*y) * 1/sqrt(2) + (1-etaA)*(1-etaBS) ) ]
-
-                
-                ### CHSH-ZX ##
-                if chsh_or_zx == 'zx':
-                    if x == y:
-                        equalities += [A[x]*D[y] - ( etaA*etaBL +  (1-etaA)*(1-etaBL) )]
-                    else:
-                        equalities+= [A[x]*D[y]]
-                
-                ## CHSH-CHSH ##
-                if chsh_or_zx == 'chsh':
-                    equalities += [A[x]*D[y] -  ( etaA*etaBL* (-1)**(x*y) * 1/sqrt(2) + (1-etaA)*(1-etaBL) ) ]    
-    
-    else:
-        # A_obs,Asq_obs,B_obs,Bsq_obs,D_obs,Dsq_obs,AB_obs,ABsq_obs,AsqB_obs,AsqBsq_obs,\
-        #     AD_obs,ADsq_obs,AsqD_obs,AsqDsq_obs = get_obs_correlators(p_obs)
-        for x in range(nX):
-            equalities += [A[x] ]
-            equalities += [A[x]**2 - etaA]
-            
-            # equalities += [A[x] - A_obs[x]]
-            # equalities += [A[x]**2 - Asq_obs[x]]
-            
-            
-        for y in range(nY):
-            equalities += [B[y] ]
-            equalities += [B[y]**2 - etaBS]
-            equalities += [D[y]]
-            equalities += [D[y]**2 - etaBL]
-            
-            # equalities += [B[y] -  B_obs[y]]
-            # equalities += [B[y]**2 - Bsq_obs[y] ]
-            # equalities += [D[y] -  D_obs[y] ]
-            # equalities += [D[y]**2 - Dsq_obs[y]]
-            
-            
-        for x in range(nX):
-            for y in range(nY):
-                equalities += [A[x]*B[y] -( (-1)**(x*y) *etaA*etaBS*1/(sqrt(2)))]
-                equalities += [A[x]*B[y]**2 ]
-                equalities += [A[x]**2*B[y] ]
-                equalities += [A[x]**2*B[y]**2 - etaA*etaBS]
-                
-                ### CHSH-ZX ##
-                if chsh_or_zx == 'zx':
-                    if x == y:
-                        equalities += [A[x]*D[y] - etaA*etaBL]
-                    else:
-                        equalities+= [A[x]*D[y]]
-                
-                ## CHSH-CHSH ##
-                if chsh_or_zx == 'chsh':
-                    equalities += [A[x]*D[y] - ( (-1)**(x*y) *etaA*etaBL*1/(sqrt(2)))]
-    
-                equalities += [A[x]*D[y]**2 ]
-                equalities += [A[x]**2*D[y]]
-                equalities += [A[x]**2*D[y]**2 - etaA*etaBL]
-                
-                # equalities += [A[x]*B[y] - AB_obs[x,y]]
-                # equalities += [A[x]*B[y]**2 - ABsq_obs[x,y]]
-                # equalities += [A[x]**2*B[y] - AsqB_obs[x,y]]
-                # equalities += [A[x]**2*B[y]**2 - AsqBsq_obs[x,y]]
-                
-                # equalities += [A[x]*D[y] - AD_obs[x,y]]
-                # equalities += [A[x]*D[y]**2 - ADsq_obs[x,y]]
-                # equalities += [A[x]**2*D[y] - AsqD_obs[x,y]]
-                # equalities += [A[x]**2*D[y]**2 - AsqDsq_obs[x,y]]
-
-
-        
-    return equalities
-
-def get_p_ideal():
-    MA=np.zeros((nA,nX), dtype=object)
-    MB=np.zeros((nB,nY), dtype=object)
-    [MA[0,0], MA[1,0], MA[0,1], MA[1,1]] = [(I+Z)/2 ,(I-Z)/2, (I+X)/2, (I-X)/2]  
-    [MB[0,0], MB[1,0], MB[0,1], MB[1,1]] = [(I+ZR)/2 ,(I-ZR)/2, (I+XR)/2, (I-XR)/2]
-
-    bchsh = 0
-    pchsh = np.zeros((nA,nB,nX,nY))
-    psteer= np.zeros((nA,nB,nX,nY))
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    pchsh[a,b,x,y] = np.trace(phip @ np.kron(MA[a,x],MB[b,y]))
-                    bchsh += (-1)**(a+b+(x)*(y)) * pchsh[a,b,x,y]
-                    psteer[a,b,x,y] = np.trace(phip @ np.kron(MA[a,x],MA[b,y]))
-
-    return pchsh,psteer,bchsh
-[I, X, Y, Z] = [qtp.qeye(2)[:], qtp.sigmax()[:], qtp.sigmay()[:], qtp.sigmaz()[:]]
-[ZR , XR] = [(Z+X)/sqrt(2) , (Z-X)/sqrt(2)]
-
-phip=qtp.bell_state('00')[:] @ qtp.bell_state('00')[:].T.conj()
-
-def get_obs_correlators(p_obs):
-    p, pA, pB = ind_elements(p_obs,'ncpol')
-        
-    
-    A_obs = np.zeros(nX, dtype=object)
-    for x in range(nX):
-        for a in range(nA):
-            A_obs[x] = A_obs[x] + (-1)**a * pA[a,x]
-            
-    Asq_obs = np.zeros(nX, dtype=object)
-    for x in range(nX):
-        for a in range(nA):
-            Asq_obs[x] = Asq_obs[x] +  pA[a,x]
-    
-    B_obs = np.zeros(nY, dtype=object)
-    for y in range(nY):
-        for b in range(nB):    
-            B_obs[y] = B_obs[y] +(-1)**b * pB[b,y,0]
- 
-    Bsq_obs = np.zeros(nY, dtype=object)
-    for y in range(nY):
-        for b in range(nB):    
-            Bsq_obs[y] = Bsq_obs[y] + pB[b,y,0]    
-    
-    D_obs = np.zeros(nY, dtype=object)
-    for y in range(nY):
-        for b in range(nB):    
-            D_obs[y] = D_obs[y] + (-1)**b * pB[b,y,1]
-   
-    Dsq_obs = np.zeros(nY, dtype=object)
-    for y in range(nY):
-        for b in range(nB):    
-            Dsq_obs[y] = Dsq_obs[y] + pB[b,y,1]
-            
-    AB_obs = np.zeros((nX,nY), dtype=object)
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    AB_obs[x,y] = AB_obs[x,y] + (-1)**a * (-1)**b * p[a,b,x,y,0]
-
-    ABsq_obs = np.zeros((nX,nY), dtype=object)
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    ABsq_obs[x,y] = ABsq_obs[x,y] + (-1)**a  * p[a,b,x,y,0]
-
-    AsqB_obs = np.zeros((nX,nY), dtype=object)
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    AsqB_obs[x,y] = AsqB_obs[x,y] + (-1)**b * p[a,b,x,y,0]
-    
-    AsqBsq_obs = np.zeros((nX,nY), dtype=object)
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    AsqBsq_obs[x,y] = AsqBsq_obs[x,y] + p[a,b,x,y,0]
-                    
-
-    AD_obs = np.zeros((nX,nY), dtype=object)
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    AD_obs[x,y] = AD_obs[x,y] + (-1)**a * (-1)**b * p[a,b,x,y,1]
-
-    ADsq_obs = np.zeros((nX,nY), dtype=object)
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    ADsq_obs[x,y] = ADsq_obs[x,y] + (-1)**a  * p[a,b,x,y,1]
-
-    AsqD_obs = np.zeros((nX,nY), dtype=object)
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    AsqD_obs[x,y] = AsqD_obs[x,y] + (-1)**b * p[a,b,x,y,1]
-    
-    AsqDsq_obs = np.zeros((nX,nY), dtype=object)
-    for x in range(nX):
-        for y in range(nY):
-            for a in range(nA):
-                for b in range(nB):
-                    AsqDsq_obs[x,y] = AsqDsq_obs[x,y] + p[a,b,x,y,1]
-
-    return A_obs,Asq_obs,B_obs,Bsq_obs,D_obs,Dsq_obs,AB_obs,ABsq_obs,AsqB_obs,AsqBsq_obs,\
-        AD_obs,ADsq_obs,AsqD_obs,AsqDsq_obs
-"""
+plt.plot(etaA_list,etaBL_list,'r-' , linewidth=2)
+plt.grid(color='r', linestyle='-', linewidth=1)
